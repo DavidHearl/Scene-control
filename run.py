3fd395e2bc9fb962bfdf87e77368bc572a2c3c02
DavidHearl <david.hearl@outlook.com>
2022-11-28 16:50:39 +0000

    removed waste time function

--- run.py
+++ run.py
@@ -2,16 +2,6 @@ import pyautogui as gui
 import time
 import math
 
-def waste_time():
-    R = 200
-    (x,y) = gui.size()
-    (X,Y) = gui.position(x/2,y/2)
-    gui.moveTo(X+R,Y)
-
-    for i in range(90):
-        if i%6==0:
-            gui.moveTo(X+R*math.cos(math.radians(i*4)),Y+R*math.sin(math.radians(i*4)))
-
 # Navigate to windows search
 window_search = gui.locateCenterOnScreen("windows-search.PNG")
 gui.moveTo(window_search, duration=0.5)
@@ -21,9 +11,7 @@ time.sleep(1)
 gui.hotkey("enter")
 
 # Exit Update menu
-for x in range(8):
-    waste_time()
-
+time.sleep(11)
 update_exit = gui.locateCenterOnScreen("scene-update-exit.PNG")
 gui.moveTo(update_exit, duration=1)
 gui.click()
