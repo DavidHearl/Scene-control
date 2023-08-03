0704ce00beaeb4c41ae40fcd1a8bb6b38d84a817
DavidHearl <david.hearl@outlook.com>
2023-08-03 09:48:49 +0100

    fixed box styling issues

--- webpage/script.js
+++ webpage/script.js
@@ -10,12 +10,12 @@ fetch('../data/scan_database.json')
             const areas = shipInfo.areas;
 
             const shipElement = document.createElement('div');
-            shipElement.className = 'container mb-4';
-            
+            shipElement.className = 'container-fluid ship-section mb-4';
+
             // Create a row for ship name and number
             const shipRow = document.createElement('div');
             shipRow.className = 'row mb-2';
-            
+
             const shipNameElement = document.createElement('strong');
             shipNameElement.className = 'col';
             shipNameElement.textContent = `${shipName} (Number: ${shipNumber})`;
@@ -28,16 +28,23 @@ fetch('../data/scan_database.json')
 
                 const areaElement = document.createElement('div');
                 areaElement.className = 'area row mb-2'; // Use row class for area
-                
+
                 const areaNameElement = document.createElement('strong');
-                areaNameElement.className = 'area-name col'; // Use col class for area name
+                areaNameElement.className = 'col-lg-4'; // Use col class for area name
                 areaNameElement.textContent = area;
                 areaElement.appendChild(areaNameElement);
 
                 for (const subcategory in areaDetails) {
+                    const subcategoryElement = document.createElement('div');
+                    subcategoryElement.className = 'col-md-6 col-lg-1 area-subcategory'; // Use col-2 class for subcategory
+
                     const subcategoryBox = document.createElement('div');
                     subcategoryBox.className = `area-box ${areaDetails[subcategory]}`;
-                    areaElement.appendChild(subcategoryBox);
+                    subcategoryBox.textContent = subcategory; // Set the description as the text content
+
+                    subcategoryElement.appendChild(subcategoryBox);
+
+                    areaElement.appendChild(subcategoryElement);
                 }
 
                 shipElement.appendChild(areaElement);
