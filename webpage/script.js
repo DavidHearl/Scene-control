ab134f41cb36afe664fe9ffaf6ae7716cefbae42
DavidHearl <david.hearl@outlook.com>
2023-08-07 15:01:19 +0100

    optimised JS, fixed quirky CSS behavior

--- webpage/script.js
+++ webpage/script.js
@@ -1,7 +1,8 @@
-// Fetch the data and populate the ships and companies filter dropdowns
+// Fetch the data from the "scan_database.json"
 fetch('../data/scan_database.json')
     .then(response => response.json())
     .then(data => {
+        // Database search filters
         const shipFilter = document.getElementById('ship-filter');
         const companyFilter = document.getElementById('company-filter');
         const numberSearch = document.getElementById('number-search');
@@ -26,8 +27,9 @@ fetch('../data/scan_database.json')
             }
         }
 
-        // Sort shipNames alphabetically
+        // Sort shipNames & company names alphabetically
         shipNames.sort();
+        companyNames.sort();
 
         // Populate ship filter dropdown options
         for (const shipName of shipNames) {
@@ -52,6 +54,7 @@ fetch('../data/scan_database.json')
         applyFilters();
 
         function applyFilters() {
+            // Variables containing filter values
             const selectedShip = shipFilter.value;
             const selectedCompany = companyFilter.value;
             const searchNumber = numberSearch.value.toLowerCase();
@@ -64,7 +67,6 @@ fetch('../data/scan_database.json')
                 const shipName = ship;
                 const shipNumber = shipInfo.number;
                 const companyName = shipInfo.company;
-                const areas = shipInfo.areas;
 
                 // Check if the ship and company match the selected filters
                 const shipMatched = !selectedShip || selectedShip === shipName;
@@ -74,20 +76,23 @@ fetch('../data/scan_database.json')
                 if (shipMatched && companyMatched && numberMatched) {
                     const areas = shipInfo.areas;
 
+                    // Create a div/container for each ship
                     const shipElement = document.createElement('div');
-                    shipElement.className = 'container-fluid ship-section mb-4';
+                    shipElement.className = 'container ship-section';
 
-                    // Create a row for ship name and number
+                    // Create a row for ship name, number and area count
                     const shipRow = document.createElement('div');
-                    shipRow.className = 'row mb-2';
+                    shipRow.className = 'row ship-title';
 
+                    // Create the title 
                     const shipNameElement = document.createElement('div');
-                    shipNameElement.className = 'col';
+                    shipNameElement.className = 'col-4';
                     const shipNameHeader = document.createElement('h3');
                     shipNameHeader.textContent = shipName;
                     shipNameElement.appendChild(shipNameHeader);
                     shipRow.appendChild(shipNameElement);
 
+                    // Create the contract number
                     const shipNumberElement = document.createElement('div');
                     shipNumberElement.className = 'col';
                     const shipNumberHeader = document.createElement('h3');
@@ -95,6 +100,7 @@ fetch('../data/scan_database.json')
                     shipNumberElement.appendChild(shipNumberHeader);
                     shipRow.appendChild(shipNumberElement);
 
+                    // Create the area count
                     const areaCountElement = document.createElement('div');
                     areaCountElement.className = 'col';
                     const areaCountText = document.createElement('h3');
@@ -104,63 +110,59 @@ fetch('../data/scan_database.json')
 
                     shipElement.appendChild(shipRow);
 
+                    // Create an hr element under the title block
+                    const titleUnderline = document.createElement('hr');
+                    titleUnderline.className= 'title-underline';
+                    shipElement.appendChild(titleUnderline);
+
                     for (const area in areas) {
                         const areaDetails = areas[area];
                         const subcategoryValues = Object.values(areaDetails);
                         const isAllSameValue = subcategoryValues.every(value => value === subcategoryValues[0]);
-        
+
                         const areaElement = document.createElement('div');
-                        areaElement.className = 'area row mb-2';
-        
+                        areaElement.className = 'row area';
+
                         const areaNameElement = document.createElement('div');
-                        areaNameElement.className = 'col-lg-4';
-        
-                        const areaNameList = document.createElement('ul');
-                        areaNameList.className = '';
-        
-                        const areaNameListItem = document.createElement('li');
-                        areaNameList.className = 'area-name';
-                        areaNameListItem.textContent = area;
-        
-                        areaNameList.appendChild(areaNameListItem);
-                        areaNameElement.appendChild(areaNameList);
+                        areaNameElement.className = 'col-4 area-title';
+
+                        const areaNameParagraph = document.createElement('p');
+                        areaNameParagraph.className = 'area-name';
+                        areaNameParagraph.textContent = area;
+                        
+                        areaNameElement.appendChild(areaNameParagraph);
                         areaElement.appendChild(areaNameElement);
-        
+
                         if (isAllSameValue) {
                             const subcategoryElement = document.createElement('div');
-                            subcategoryElement.className = 'col-lg-8 area-subcategory';
+                            subcategoryElement.className = 'col-8';
                             const status = subcategoryValues[0];
-                            const statusClass = status.replace(/\s/g, ''); // Remove spaces from status for class name
-                            subcategoryElement.className = `col-lg-8 area-box ${status === 'No Data' ? 'NoData' : statusClass}`;
+                            subcategoryElement.className = `col-8 area-box-large ${status === 'NoData' ? 'NoData' : status}`;
                             subcategoryElement.textContent = `All ${status}`;
                             areaElement.appendChild(subcategoryElement);
                         } else {
                             for (const subcategory in areaDetails) {
                                 const subcategoryElement = document.createElement('div');
-                                subcategoryElement.className = 'col-md-6 col-lg-1 area-subcategory';
-
+                                subcategoryElement.className = 'col-md-6 col-lg-1 progress-box';
+                        
                                 const subcategoryBox = document.createElement('div');
-
+                        
                                 const status = areaDetails[subcategory];
-                                const statusClass = status.replace(/\s/g, ''); // Remove spaces from status for class name
-
+                        
                                 // Set class based on status, use "NoData" for [No Data]
-                                subcategoryBox.className = `area-box ${status === 'No Data' ? 'NoData' : statusClass}`;
+                                subcategoryBox.className = `area-box ${status === 'NoData' ? 'NoData' : status}`;
                                 subcategoryBox.textContent = subcategory; // Set the description as the text content
-
+                        
                                 subcategoryElement.appendChild(subcategoryBox);
-
+                        
                                 areaElement.appendChild(subcategoryElement);
                             }
-                        }
-
+                        }                        
                         shipElement.appendChild(areaElement);
                     }
-
                     shipDetailsDiv.appendChild(shipElement);
                 }
             }
         }
-
     })
     .catch(error => console.error('Error fetching JSON:', error));
