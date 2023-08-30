741bafc20b8dd2f4e81329c3ef766cc163b54a4d
DavidHearl <david.hearl@outlook.com>
2023-08-30 17:07:02 +0100

    updated database, added back button, fixed problem with the operation not progressing

--- operations/start_operations.py
+++ operations/start_operations.py
@@ -264,22 +264,31 @@ class InitialProcedures:
             drop_down_btn_pos = pyautogui.center(drop_down_btn)
             drop_down_btn_pos = (drop_down_btn.left - 20, drop_down_btn_pos[1])
 
-            pyautogui.moveTo(drop_down_btn_pos)
+            pyautogui.moveTo(drop_down_btn_pos, duration=0.5)
             pyautogui.click()
+            time.sleep(0.5)
 
             # Paste the link
             pyautogui.typewrite(processed_folder_path)
+            time.sleep(0.5)
+            pyautogui.press('enter')
+            time.sleep(0.5)
 
             # Click select folder
             select_folder_btn = pyautogui.locateOnScreen('items/select-folder.png')
             select_folder_btn_ctr = pyautogui.center(select_folder_btn)
-
-            pyautogui.moveTo(select_folder_btn_ctr)
+            pyautogui.moveTo(select_folder_btn_ctr, duration=0.5)
             pyautogui.click()
 
+            # Return to main page
+            back_btn = pyautogui.locateOnScreen('items/back.PNG')
+            back_btn_ctr = pyautogui.center(back_btn)
+            pyautogui.moveTo(back_btn_ctr, duration=0.5)
+            pyautogui.click()
+
+            # Click project transfer button
             project_transfer_btn = pyautogui.locateOnScreen('items/project-transfer.PNG')
             project_transfer_btn_ctr = pyautogui.center(project_transfer_btn)
-
             pyautogui.moveTo(project_transfer_btn_ctr, duration=0.5)
             pyautogui.click()
 
--- testing.py
+++ testing.py
@@ -1,4 +1,48 @@
 import pyautogui
+import time
 
-mouse_pos = pyautogui.position()
-print(mouse_pos)
+
+# Get mouse position
+def get_mouse_position():
+    mouse_pos = pyautogui.position()
+    print(mouse_pos)
+
+
+def code():
+    processed_folder_path = "E:\XXX - Test - Processed"
+
+    # Click to the left of the drop down arrow
+    drop_down_btn = pyautogui.locateOnScreen('items/previous-location.png')
+    drop_down_btn_pos = pyautogui.center(drop_down_btn)
+    drop_down_btn_pos = (drop_down_btn.left - 20, drop_down_btn_pos[1])
+
+    pyautogui.moveTo(drop_down_btn_pos, duration=0.5)
+    pyautogui.click()
+    time.sleep(0.5)
+
+    # Paste the link
+    pyautogui.typewrite(processed_folder_path)
+    time.sleep(0.5)
+    pyautogui.press('enter')
+    time.sleep(0.5)
+
+    # Click select folder
+    select_folder_btn = pyautogui.locateOnScreen('items/select-folder.png')
+    select_folder_btn_ctr = pyautogui.center(select_folder_btn)
+    pyautogui.moveTo(select_folder_btn_ctr, duration=0.5)
+    pyautogui.click()
+
+    # Return to main page
+    back_btn = pyautogui.locateOnScreen('items/back.PNG')
+    back_btn_ctr = pyautogui.center(back_btn)
+    pyautogui.moveTo(back_btn_ctr, duration=0.5)
+    pyautogui.click()
+
+    # Click project transfer button
+    project_transfer_btn = pyautogui.locateOnScreen('items/project-transfer.PNG')
+    project_transfer_btn_ctr = pyautogui.center(project_transfer_btn)
+    pyautogui.moveTo(project_transfer_btn_ctr, duration=0.5)
+    pyautogui.click()
+
+
+code()
