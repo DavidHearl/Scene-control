5a1f9a513815c5658a0ac5377526e025157605d4
DavidHearl <david.hearl@outlook.com>
2023-09-01 13:16:17 +0100

    Initial Django commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn scene-control.wsgi
---
+++ env.py
@@ -0,0 +1,5 @@
+import os
+
+os.environ["SECRET_KEY"] = 'django-insecure-wvdxc5zi&0vr4*l2#!wrmwm60nqy6uhky4v=)y9kst*k)-jr7@'
+os.environ["DATABASE_URL"] = 'postgres://oyitbhqifvvhqa:98846d07154c1e4d0df21c3d385319ddf8d9404f63a7952c05e243ce63489a91@ec2-54-78-142-10.eu-west-1.compute.amazonaws.com:5432/ddu7r5dc47v3n3'
+os.environ["CLOUDINARY_URL"] = 'cloudinary://936271935583843:_12f1rNokgM04WjvAOH2H14tSyw@dsvgub3ba'
